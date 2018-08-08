6a210d55a11636791bba18da0308527c462efc76
dodok8 <dodok8@gmail.com>
2018-08-09 00:09:46 +0900

    1328번 소스코드 형식에 맞게 수정

--- Baekjoon/1328_고층 빌딩v1.py
+++ Baekjoon/1328_고층 빌딩v1.py
@@ -1,28 +1,11 @@
-# 방법 자체가 잘못 된 듯
-import sys
-sys.setrecursionlimit(100000)
-combiList = [[0 for _ in range(100)] for __ in range(100)]
-def combi(n,r):
-    if n == r or r == 0:
-        combiList[n][r] = 1
-    if combiList[n][r] == 0:
-        combiList[n][r] = (n-r+1)/r*combi(n,r-1)
-    return combiList[n][r]
-
+# https://www.acmicpc.net/problem/1328
+# 10번 시도 2번 메모리 초과 5번 런타임 에러
+# 43236 KB / 804 ms
 N,L,R = map(int,input().split())
 aList = [[[0 for _ in range(R+1)]for __ in range(L+1)]for ___ in range(N+1)]
-
-def A(n,l,r):
-    if aList[n][l][r] != 0:
-        pass
-    else :
-        if l==n or n==r :
-            aList[n][l][r] = 1
-        else:
-            sum = 0
-            for i in range(l,n-r+2):
-                sum =sum + combi(n-1,i-1)*A(i,l,1)*A(n-i+1,1,r)%1000000007
-            aList[n][l][r] = sum
-    return aList[n][l][r]
-
-print(int(A(N,L,R)%1000000007))
+aList[1][1][1] = 1
+for i in range(2,N+1):
+    for j in range(1,L+1):
+        for k in range(1,R+1):
+                aList[i][j][k] = aList[i-1][j][k]*(i-2)%1000000007+aList[i-1][j][k-1]%1000000007+aList[i-1][j-1][k]%1000000007
+print(aList[N][L][R]%1000000007)
