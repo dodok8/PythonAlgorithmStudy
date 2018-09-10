96fa1183b0d1221e21f778ce42f10df16a478d54
Kim, Hyeonseo <dodok8@gmail.com>
2018-09-10 22:38:26 +0900

    Chessboard Game, Again 전체적인 구현 수정 필요

--- Hacekerrank/Geamtheory_Chessboard Game,Again!_v1.py
+++ Hacekerrank/Geamtheory_Chessboard Game,Again!_v1.py
@@ -1,11 +1,13 @@
-def mex(inList):
-    if 0 not in inList:
+import sys
+
+def mex(*args):
+    if 0 not in args:
         return 0
-    elif 1 not in inList:
+    elif 1 not in args:
         return 1
-    elif 2 not in inList:
+    elif 2 not in args:
         return 2
-    elif 3 not in inList:
+    elif 3 not in args:
         return 3
     else :
         return 4
@@ -18,6 +20,22 @@ g[0][1] = 2
 g[0][1] = 2
 g[1][2] = 2
 g[2][1] = 1
+bList = list
 for i in range(3,15):
     for j in range(3,15):
-        g[i][j] = mex(list(g[i-2][y-1],g[i-2][j+1],g[i+1][j-2],g[i-1][j-2])
+        g[i][j] = mex(g[i-2][j-1],g[i-2][j+1],g[i+1][j-2],g[i-1][j-2])
+
+aList = list()
+
+for T in range(int(sys.stdin.readline())):
+    for k in range(int(sys.stdin.readline())):
+        x, y = map(int(sys.stdin.readline()).split())
+        aList.append(g[x][y])
+    b = aList.pop
+    for i in aList:
+        b = b^i
+    if b==0:
+        print('Second')
+    else :
+        print('First')
+    del aList[:]
