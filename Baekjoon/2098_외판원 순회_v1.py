d874207413fa2bd118f9cd823a07026113171f04
Kim, Hyeonseo <dodok8@gmail.com>
2018-09-15 20:18:54 +0900

    2098번 Index out of range 오류

--- Baekjoon/2098_외판원 순회_v1.py
+++ Baekjoon/2098_외판원 순회_v1.py
@@ -1,11 +1,35 @@
 # https://www.acmicpc.net/problem/2098
 
+MAX_INF = 100000000
 import sys
 
 n = int(sys.stdin.readline())
-w = list()
-cost = 0
-for _ in range(n):
-    w.append(list(map(int(),sys.stdin.readline().split())))
+w = [[] for _ in range(n)]
+dpList = [[None for __ in range(1<<n)] for _ in range(n)]
+for j in range(1<<n):
+    if j==0:
+        dpList[0][1] = 0
+    else :
+        dpList[0][j] = MAX_INF
+for i in range(n):
+    for j in map(int, sys.stdin.readline().split()):
+        if j != 0:
+            w.append(j)
+        else :
+            w.append(MAX_INF)
 
-print(cost)
+def DP(u,a) :
+    if dpList[u][a] is not None:
+        return dpList[u][a]
+    else :
+        compareList = list()
+        for i in range(n):
+            if i != u and ((a >> i) & 1):
+                compareList.append(DP(i,a)+w[i][u])
+        dpList[u][a] = min(compareList)
+        return dpList[u][a]
+
+compareList = list()
+for i in range(1,n):
+    compareList.append(DP(i,(1<<n)-1)+w[i][0])    
+print(min(compareList))
