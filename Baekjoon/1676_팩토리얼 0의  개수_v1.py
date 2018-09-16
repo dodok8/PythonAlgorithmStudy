8119d4c3d153580b1338f4695eb4c9c2f1334682
Kim, Hyeonseo <dodok8@gmail.com>
2018-09-17 01:13:31 +0900

    1676 풀이 완료

---
+++ Baekjoon/1676_팩토리얼 0의  개수_v1.py
@@ -0,0 +1,10 @@
+#https://www.acmicpc.net/problem/1676
+#1번 틀림
+from sys import stdin
+n = int(stdin.readline())
+squares = 5
+num0 = 0
+while squares < n :
+    num0 = num0 + n//squares
+    squares= squares*5
+print(num0)
