0cde8a99998688a68575a7e202ccacf54eb369a3
Kim, Hyeonseo <dodok8@gmail.com>
2018-09-18 00:55:23 +0900

    2098번 출력이 없다 왜이러지

--- Baekjoon/2098_외판원 순회_v2.py
+++ Baekjoon/2098_외판원 순회_v2.py
@@ -1,30 +1,39 @@
 # https://www.acmicpc.net/problem/2098
 
 from math import inf
-from sys import stdin
+from sys import stdin,setrecursionlimit
 
-def DP(now : int, visited: int) -> int :
+setrecursionlimit(10**9)
+
+def DP(now, visited):
     global dpList, visitedAll,w
-    if dpList[now][visited] is not None:
+    if now == 0:
+        #시작 점 초기화
+        if visited == 1:
+            dpList[now][visited] = 0
+        #시작한 상황인데 이미 방문한 곳이 1이상인 이상한 경우 제외
+        else :
+            dpList[now][visited] = inf
+    if dpList[now][visited] != None:
         pass
     else:
         #여기서 부터는 아직 방문 안한 거
-        compareList = list()
-        vistedExceptNow = visted & (~now)
-        for i in range(vistedExceptNow):
-            if (vistedExceptNow >> i) & 1:
+        tempVal = inf
+        visitedExceptNow = visited & (~now)
+        for i in range(n):
+            if (visitedExceptNow >> i) & 1:
                 before = i
-                compareList.append(DP(before,vistedExceptNow)+w[before][now])
-        dpList[now][visited] = min(compareList)
+                tempVal = min(tempVal, DP(before,visitedExceptNow)+w[before][now])
+        dpList[now][visited] = tempVal
     return dpList[now][visited]
+
 n = int(stdin.readline())
 w = list()
 for _ in range(n):
     w.append([inf if int(i) == 0 else int(i) for i in stdin.readline().split()])
 dpList = [[None for _ in range(1 << n)] for __ in range(n)]
 visitedAll = (1 << n) - 1
-compareList = list()
+aList = list()
 for i in range(1,n):
-    compareList.append(DP(i,visitedAll)+w[i][1])
-print(min(compareList))
-
+    aList.append(DP(i,visitedAll)+w[i][0])
+print(type(min(aList)))
