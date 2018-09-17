a226ac1226e6a0b7b6bb1c9d5cb6a32a87dd4c94
Kim, Hyeonseo <dodok8@gmail.com>
2018-09-17 15:17:08 +0900

    2748번 풀이 완료

--- Baekjoon/2748_피보나치 수2_v1.py
+++ Baekjoon/2748_피보나치 수2_v1.py
@@ -1,21 +1,23 @@
 #https://www.acmicpc.net/problem/2748
-
+# PyPy3 117076KB 108ms
 from sys import stdin
 class FbList:
     def __init__(self, n):
         self.fbList = [-1 for i in range(n+1)]
         self.fbList[0] = 0
         self.fbList[1] = 1
-        self.size = len(self.fbList)
-    def getValue(self,n):
-        if n > self.size:
-            self.fbList.extend([-1 for i in range(n-self.size)])
-            self.get(n)
-        else :
-            if self.fbList[n] == -1:
-                self.fbList[n] = self.fbList[n-1] + self.fbList[n-2]
-            return self.fbList[n]
-    def getValue(self):
-        return self.fbList[len(fbList-1)]
+        self.size = int(len(self.fbList))
+    def getValue(self, *args):
+        if args:
+            n = args[0]
+            if n > self.size:
+                self.fbList.extend([-1 for i in range(n-self.size)])
+                self.get(n)
+            else :
+                if self.fbList[n] == -1:
+                    self.fbList[n] = self.getValue(n-1) + self.getValue(n-2)
+                return self.fbList[n]
+        else:
+            return self.getValue(len(self.fbList)-1)
 fibo = FbList(int(stdin.readline()))
-fibo.getValue()
+print(fibo.getValue())
