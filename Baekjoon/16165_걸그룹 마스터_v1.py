1e5bc3a19728ea955546d2025cfc617826b6b366
Kim, Hyeonseo <dodok8@gmail.com>
2018-10-01 20:55:49 +0900

    16165 입력부 작성

--- Baekjoon/16165_걸그룹 마스터_v1.py
+++ Baekjoon/16165_걸그룹 마스터_v1.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/16165
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+girl_groups = dict()
+for i in range(N):
+    group_name = stdin.readline()
+    girl_groups[group_name] = list()
+    for i in range(int(stdin.readline())):
+        girl_groups[group_name].append(stdin.readline())
+for i in range(M):
+    
+
