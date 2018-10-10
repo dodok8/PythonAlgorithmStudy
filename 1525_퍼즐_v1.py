e9aa2f0f7127358a6fb799e70285ecb692d60f11
Kim, Hyeonseo <dodok8@gmail.com>
2018-10-10 22:15:58 +0900

    1525 퍼즐 BFS 완성

--- 1525_퍼즐_v1.py
+++ 1525_퍼즐_v1.py
@@ -1,5 +1,5 @@
 '''
-링크: 
+링크: https://www.acmicpc.net/problem/1525
 런타임 에러:
 틀렸습니다:
 메모리 초과:
@@ -7,3 +7,39 @@
 Python3  KB  ms
 PyPy3  KB  ms
 '''
+from sys import stdin
+from collections import defaultdict as defdict
+from collections import deque
+
+board = []
+now_board_number = 0
+for i in range(3):
+    board.append(list(map(int, stdin.readline().split)))
+    now_board_number = (board[i][0]*9**(i*3+0)
+                    + board[i][1]*9**(i*3+1)
+                    + board[i][2]*9**(i*3+2))
+final_board = 42374116
+visited = defdict(lambda: False)
+visited[now_board_number] = True
+distance = [0 for _ in range(362880)] #현재 거리에서 얼마나 멀리 떨어져 있는 지 측정
+
+def BFS(start):
+    global visited, final_board, distance
+    queue = deque()
+    queue.append(start)
+    while queue:
+        now = queue.popleft()
+        connected_node = list()
+        #TODO: 복원하는 과정을 짜야한다. 복원해서 그걸 숫자를 바꿔서 넣고 
+        for i in connected_node:
+            if not visited[i]:
+                if i == final_board:
+                    visited[i] = True
+                    distance[i] = distance[now] + 1 
+                    # 도착 한거니깐 이 때까지 값 반환
+                    return distance[i]
+                else:
+                    visited[i] = True
+                    distance[i] = distance[now] + 1
+                    queue.append(i)
+    #제대로 작동할지는 모르겠는데 어찌되었든 BFS 완성
