f20d9abad88377177ed72829585b5c361425f24a
Kim, Hyeonseo <dodok8@gmail.com>
2018-10-11 11:31:28 +0900

    1525 뭔가 잘못 짰는지 엄청 오래걸린다.

--- 1525_퍼즐_v1.py
+++ 1525_퍼즐_v1.py
@@ -8,38 +8,67 @@ Python3  KB  ms
 PyPy3  KB  ms
 '''
 from sys import stdin
-from collections import defaultdict as defdict
-from collections import deque
+from collections import defaultdict, deque
 
-board = []
-now_board_number = 0
+board = list()
 for i in range(3):
-    board.append(list(map(int, stdin.readline().split)))
-    now_board_number = (board[i][0]*9**(i*3+0)
-                    + board[i][1]*9**(i*3+1)
-                    + board[i][2]*9**(i*3+2))
-final_board = 42374116
-visited = defdict(lambda: False)
-visited[now_board_number] = True
-distance = [0 for _ in range(362880)] #현재 거리에서 얼마나 멀리 떨어져 있는 지 측정
+    board.extend(map(int, stdin.readline().split()))
+temp_str = ''.join(str(i) for i in board)
+now_board_number = int(temp_str,9)
+
+def convert_9(n: int) -> str:
+    T = "012345678"
+    q, r = divmod(n, 9)
+    if q == 0:
+        return T[r]
+    else:
+        return convert_9(q) + T[r]
+
+
+def get_connected_nodes(now: int):
+    temp_board = list(convert_9(now))
+    connected_list = list()
+    swap_list = [1, -1, -3, 3]
+    try:
+        x = temp_board.index('0') #여기서 0 이 없는게 맨 앞 자리로 가서 그런거 아닐까?
+    except:
+        x = 0
+        temp_board.insert(0,'0')
+    else:
+        x = temp_board.index('0')
+    for i in swap_list:
+        if x+i < 0 or x+i >= 9:
+            pass
+        else:
+            temp_board[x], temp_board[x+i] = temp_board[x+i], temp_board[x]
+            temp_sum = ''.join(temp_board)
+            connected_list.append(int(temp_sum, 9))
+            temp_board[x], temp_board[x+i] = temp_board[x+i], temp_board[x]
+    #for i in connected_list:
+    #    print(convert_9(i))
+    return connected_list
+
 
 def BFS_final(start):
-    global visited, final_board, distance
+    visited = defaultdict(lambda: False)
+    visited[start] = True
+    final_board = 42374116
+    distance = defaultdict(lambda: 0)
     queue = deque()
     queue.append(start)
     while queue:
         now = queue.popleft()
-        connected_node = list()
-        #TODO: 복원하는 과정을 짜야한다. 복원해서 그걸 숫자를 바꿔서 넣고 
-        for i in connected_node:
+        print('now는 ', convert_9(now))
+        for i in get_connected_nodes(now):
             if not visited[i]:
                 if i == final_board:
                     visited[i] = True
-                    distance[i] = distance[now] + 1 
-                    # 도착한거니깐 이 때까지 값 반환
+                    distance[i] = distance[now] + 1
                     return distance[i]
                 else:
                     visited[i] = True
                     distance[i] = distance[now] + 1
-                    queue.append(i) #다음 now 포인트가 될 테니 큐에 넣기
-    #제대로 작동할지는 모르겠는데 어찌되었든 BFS 완성
+                    queue.append(i)  # 다음 now 포인트가 될 테니 큐에 넣기
+    # 제대로 작동할지는 모르겠는데 어찌되었든 BFS 완성
+
+print(BFS_final(now_board_number))
