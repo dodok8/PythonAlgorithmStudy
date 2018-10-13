fa241fa0d49195665a89ee5b1d472edb50d16886
Kim, Hyeonseo <dodok8@gmail.com>
2018-10-13 23:29:23 +0900

    16165번 걸그룹 마스터 풀이 완료

--- Baekjoon/16165_걸그룹 마스터_v1.py
+++ Baekjoon/16165_걸그룹 마스터_v1.py
@@ -1,23 +1,27 @@
 '''
 https://www.acmicpc.net/problem/16165
-PyPy3 
+출력 형식 오류: 1번
+런타임 에러: 1번
+틀렸습니다: 1번
+PyPy3 117592KB 108ms
 '''
-from sys import stdin
-
-N, M = map(int, stdin.readline().split())
+N, M = map(int, input().split())
 girl_groups = dict()
 for i in range(N):
-    group_name = stdin.readline()
+    group_name = input()
     girl_groups[group_name] = list()
-    for i in range(int(stdin.readline())):
-        girl_groups[group_name].append(stdin.readline())
+    for i in range(int(input())):
+        girl_groups[group_name].append(input())
+    girl_groups[group_name].sort()
 for i in range(M):
-    quiz = int(stdin.readline())
+    in_name = input()
+    quiz = int(input())
     if quiz == 1:
-        question = 
-        print('hello')
-    else:
-        # 어느 그룹인지 출력
         for i in girl_groups.keys():
-            if ??? in girl_groups[i]:
-                prrint(i)
+            if in_name in girl_groups[i]:
+                print(i)
+                continue
+        # 어디 소속인지 출력
+    else:
+        for i in girl_groups[in_name]:
+            print(i)
