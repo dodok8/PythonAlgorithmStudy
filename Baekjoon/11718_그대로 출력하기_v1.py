df7ae9591be0b0e00bc1ca5400ec6b4bc3d59231
Kim, Hyeonseo <dodok8@gmail.com>
2019-01-22 21:43:27 +0900

    11718 그대로 출력하기 풀이 완료

---
+++ Baekjoon/11718_그대로 출력하기_v1.py
@@ -0,0 +1,14 @@
+'''
+링크: https://www.acmicpc.net/problem/11718
+런타임 에러:
+틀렸습니다: 1
+메모리 초과:
+시간초과:
+Python3  KB  ms
+PyPy3  KB  ms
+'''
+
+import sys
+
+in_word = sys.stdin.read()
+sys.stdout.write(in_word)
